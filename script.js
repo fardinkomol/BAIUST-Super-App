7ed71b03ea0993864cf8bb3191547316dd08d2d4
Fardin Komol <fardinalam2020@gmail.com>
2023-11-27 02:04:19 +0600

    fontsize js

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const messages = document.querySelector('.messages');
 const message = messages.querySelectorAll('.message');
 const messageSearch = document.querySelector('#message-search');
 
+// Theme
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
+const colorPlette = document.querySelectorAll('.choose-color span');
+
 // ------------------------------------------Sidebar-------------------------------------
 
 // Remove active class from all menu items
@@ -57,3 +64,78 @@ messagesNotification.addEventListener('click', () => {
         messages.style.boxShadow = 'none';
     }, 2000);
 })
+
+
+
+// ------------------------------------------Theme/Display customization-------------------------------------
+
+// open modal
+const openThemeModal = () => {
+    themeModal.style.display = 'grid';
+}
+
+// close modal on cilck
+const closeThemeModal = (e) => {
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display = 'none';
+    }
+}
+
+// close modal
+themeModal.addEventListener('click', closeThemeModal);
+
+theme.addEventListener('click', openThemeModal);
+
+
+
+// ------------------Fonts------------------------------
+
+// remove active class from spans or font size selectors
+const removeSizeSelector = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove('active');
+    })
+}
+
+fontSizes.forEach(size => {
+   
+
+    size.addEventListener('click',  () => {
+        removeSizeSelector();
+        let fontSize;
+        size.classList.toggle('active');
+
+        if(size.classList.contains('font-size-1')){
+            fontSize = '10px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '5.4rem');
+        }
+        else if (size.classList.contains('font-size-2')){
+            fontSize = '13px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '-7rem');
+        }
+        else if (size.classList.contains('font-size-3')){
+            fontSize = '16px';
+            root.style.setProperty('----sticky-top-left', '-2rem');
+            root.style.setProperty('----sticky-top-right', '-17rem');
+        }
+        else if (size.classList.contains('font-size-4')){
+            fontSize = '19px';
+            root.style.setProperty('----sticky-top-left', '-5rem');
+            root.style.setProperty('----sticky-top-right', '-25rem');
+        }
+        else if (size.classList.contains('font-size-5')){
+            fontSize = '22px';
+            root.style.setProperty('----sticky-top-left', '-10rem');
+            root.style.setProperty('----sticky-top-right', '-33rem');
+        }
+
+        // Change font size of root html element
+    document.querySelector('html').style.fontSize = fontSize;
+    })
+   
+})
+
+
+// Change primary colors
