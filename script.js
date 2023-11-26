97780ee1434cf165ed38432f91b016f7b97b93ab
Fardin Komol <fardinalam2020@gmail.com>
2023-11-27 00:31:39 +0600

    js for chat & message

---
+++ script.js
@@ -0,0 +1,59 @@
+// Sidebar
+const menuItems = document.querySelectorAll('.menu-item');
+
+// Messages
+const messagesNotification = document.querySelector('#messeges-notification');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+// ------------------------------------------Sidebar-------------------------------------
+
+// Remove active class from all menu items
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if(item.id != 'notifications'){
+            document.querySelector('.notifications-popup').style.display = 'none';
+        }
+        else{
+            document.querySelector('.notifications-popup').style.display = 'block';
+            document.querySelector('#notifications .notification-count').style.display = 'none';
+        }
+    })
+})
+
+
+// ------------------------------------------Messages-------------------------------------
+// Searches chats
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1){
+            user.style.display = 'flex';
+        }
+        else{
+            user.style.display = 'none';
+        }
+    })
+}
+
+// Search chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+// Highlight messege-card when it is selected
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+})
